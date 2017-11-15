e39f9e3866505042a8e4091fe346b3b10e181f9c
Natalie <njseeto@users.noreply.github.com>
2017-11-15 16:14:32 +0000

    clone test working

--- src/logic.js
+++ src/logic.js
@@ -5,3 +5,22 @@
 // ------- FILTER JSON ------- //
 // input : JSON obj and outputs filtered object
 // return object
+
+const flavours = require('./flavour.json');
+
+const logicFunctions = {
+ cloneObject: (object) => {
+  return JSON.parse(JSON.stringify(object));
+},
+
+ filterJSON: (inputStr) => {
+//clone the JSON object
+
+//based on input string, search through title key in the
+//objects and output the ones that have an instance of the letter that is inputStr
+
+
+}
+
+}
+module.exports = logicFunctions;
