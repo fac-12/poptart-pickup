0c07df859569fcdf9c89d4f5357a3f3fe67d5d76
Natalie <njseeto@users.noreply.github.com>
2017-11-16 16:01:49 +0000

    filterJSON test working relates to #3

--- tests.js
+++ tests.js
@@ -11,50 +11,10 @@ test("Tape is working", (t) =>{
 
 test("Testing filterJSON function", (t) => {
   const inputStr = /^a/;
-  const obj = logicFunctions.filterJSON(inputStr);
-  const actual = (obj.items).forEach((item) => {
-    return items.title;
-  })
+  const obj = (logicFunctions.filterJSON(inputStr))[0];
+  const actual = (obj.title).charAt(0).toLowerCase();
 
   const expected = 'a';
   t.equals(actual, expected, "should return an a");
   t.end();
 })
-
-// test("Testing filterJSON function", (t) => {
-//   const actual = logicFunctions.filterJSON('a');
-//   const expected = {
-//       "id": 3795,
-//       "title": "Apple",
-//       "url": "http://poptarts.wikia.com/wiki/Flavors",
-//       "ns": 0,
-//       "quality": 0
-//     }
-//   t.equals()
-//   t.end();
-// })
-
-
-var testObj = {
-    "items": [{
-        "id": 3795,
-        "title": "Apple",
-        "url": "http://poptarts.wikia.com/wiki/Flavors",
-        "ns": 0,
-        "quality": 0
-      },
-      {
-        "id": 2093,
-        "title": "Banana",
-        "url": "http://poptarts.wikia.com/wiki/Category:Unfrosted_Flavors",
-        "ns": 14,
-        "quality": null
-      },
-      {
-        "id": 2062,
-        "title": "Carrot",
-        "url": "http://poptarts.wikia.com/wiki/Category:Discontinued_Flavors",
-        "ns": 14,
-        "quality": 0
-      }
-]}
