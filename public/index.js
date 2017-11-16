d5ff7aaff6fcc18a7e320d8f57ccc6c4ab0a787a
Jemila <jemila.abulhawa@gmail.com>
2017-11-16 12:45:55 +0000

    work on datalist options

--- public/index.js
+++ public/index.js
@@ -1,30 +1,53 @@
 // ------- EVENT LISTENER ------- //
 // output : input.value (text from field)
 var input = document.getElementById('poptart-list');
-var datalist = document.getElementById('poptart-datalist');
+var datalist = document.getElementById('poptart-data');
 
-input.addEventListener('keyup', function(event) {
-    var inputStr = event.target.value;
-    var code = event.keyCode;
-    request("flavours?inputStr=" + inputStr + "keycode=" + code);
+input.addEventListener('keypress', function(event) {
+  var inputStr = event.target.value;
+  var code = event.keyCode;
+  request("flavours?inputStr=" + inputStr + "&keycode=" + code);
 });
-
-
-
 
 // ------- RENDER TO DOM ------- //
 // input : JSON object - should add <option> inside datalist
 
-function renderData(response) {
+// var testObj = {
+//   "items": [{
+//       "id": 3845,
+//       "title": "Blue Raspberry",
+//       "url": "http://poptarts.wikia.com/wiki/Blue_Raspberry",
+//       "ns": 0,
+//       "quality": 0
+//     },
+//     {
+//       "id": 2066,
+//       "title": "Frosted Chocolate Fudge",
+//       "url": "http://poptarts.wikia.com/wiki/Frosted_Chocolate_Fudge",
+//       "ns": 0,
+//       "quality": 29
+//     },
+//     {
+//       "id": 2430,
+//       "title": "Frosted Lava Berry Explosion",
+//       "url": "http://poptarts.wikia.com/wiki/Frosted_Lava_Berry_Explosion",
+//       "ns": 0,
+//       "quality": 12
+//     }
+//   ]
+// }
 
-    response.forEach(function (item) {
-    var option = document.createElement('option');
+function renderData(responseObj) {
+  (responseObj.items).forEach((item) => {
     console.log(item.title);
-    option.value = item;
+    var option = document.createElement('option');
+    option.value = item.title;
     datalist.appendChild(option);
+  })
+  console.log("render");
 }
+renderData(responseObj)
 
-})
 // ------- CALL REQUEST ------- //
 // url = 'flavours'
 // calls render with filtered JSON object
